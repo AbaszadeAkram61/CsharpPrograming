74c2dad28e9f92fff37ced0a1ac93986d53b38dd
AbaszadeAkram61 <abaszadeakram61@gmail.com>
2026-02-01 18:18:13 +0400

    Lesson4 assignment is not ready

---
+++ lesson4/lesson4/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arrays = new int[3];
+        arrays[0] = 1;
+        arrays[1] = 10;
+        arrays[2] = 12;
+
+
+        Console.WriteLine(arrays[0] + arrays[1] + arrays[2]);
+    }
+}
