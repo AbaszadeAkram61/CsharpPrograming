054cd25dabb0e9dcc237f3da64715e168d28fcb3
AbaszadeAkram61 <abaszadeakram61@gmail.com>
2026-02-06 11:46:54 +0400

    Some tasks were also completed at HackerRank

---
+++ lesson7/lesson7/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Program.miniMaxSum(arr);
+    }
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long sum = 0;
+        int arrmin = arr.Min();
+        int arrmax = arr.Max();
+        for (int i = 0; i < arr.Count; i++)
+        {
+            sum += arr[i];
+
+        }
+        Console.WriteLine($"{sum - arrmax} {sum - arrmin}");
+
+    }
+}
---
+++ lesson7/lesson7/Program3.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson7
+{
+    internal class Program3
+    {
+    }
+}
