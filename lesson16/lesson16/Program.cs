d73c96e122d9c1f870b75fe4ec9be210270cbdff
AbaszadeAkram61 <abaszadeakram61@gmail.com>
2026-02-14 15:31:19 +0400

    Api data fetching and Thread

---
+++ lesson16/lesson16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Reflection.Metadata;
+
+internal class Program
+{
+    private static void Main1(string[] args)
+    {
+        // Write 2 methods which both accept int n as parameter, iterates from 1 to n and just write "Method1 - i" or "Method2 - i" to console. Then
+        // write 2 seperate Threads to run them
+        
+
+        Thread Method2Thread = new Thread(() => Method2(50));
+        Method2Thread.Start();
+
+        Method1(50);
+    }
+    public static void Method1(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine($"Method1-{i}");
+            Thread.Sleep(50);
+        }
+       
+    }
+
+    public static void Method2(int n) 
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine($"Method2-{i}");
+            Thread.Sleep(50);
+        }
+    }
+}
---
+++ lesson16/lesson16/Program2.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson16
+{
+    public class Program2
+    {
+        const string ApiUrl = "https://catfact.ninja/fact";
+        public static async Task Main()
+        {
+         
+            bool Iscontinue = false;
+            Console.WriteLine("CatFact ninja");
+            Console.WriteLine();
+            do
+            {
+                HttpClient client = new HttpClient();
+                var StringResult = await client.GetStringAsync(ApiUrl);
+                var Result= JsonConvert.DeserializeObject<Catfact>(StringResult);
+                Console.WriteLine(Result.Fact);
+                Console.WriteLine();
+                Console.WriteLine("Do you continue?/nTrue/nFalse");
+                Iscontinue = Convert.ToBoolean(Console.ReadLine());
+                Console.WriteLine();
+            }
+            while (Iscontinue);
+        }
+    }
+    public class Catfact
+    {
+        public string Fact {  get; set; }
+
+        public int Length {  get; set; }
+    }
+
+}
